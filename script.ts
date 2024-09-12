1398f0e1b998d753663c55a776d45e101c83ac85
Naeem Ahmed sahito <164561340+NAEEMSA123@users.noreply.github.com>
2024-09-12 09:59:37 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,11 @@
+let buttonSkills:any = document.getElementById('button-skills') as HTMLButtonElement;
+const skills = document.getElementById('skills') as HTMLElement
+
+
+ buttonSkills = addEventListener('click', ()=>{
+    if(skills.style.display === 'none'){
+        skills.style.display = 'block'
+    }else{
+        skills.style.display = 'none'
+    }
+})
